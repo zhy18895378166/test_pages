cb259ad10dab972cfd28bdabdb1494264e1d83cb
zhy18895378166 <70814310+zhy18895378166@users.noreply.github.com>
2025-01-22 02:02:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 alert("hello zhy!")
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('This is a simple HTTP server.\n');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
